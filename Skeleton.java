b60ae399bb87bb37bca740968eba8f1a8d914de9
László Mérő <laci37@gmail.com>
2015-03-14 22:24:08 +0100

    szkeleton implementáció egy része

--- Skeleton.java
+++ Skeleton.java
@@ -4,12 +4,54 @@ public class Skeleton {
 
     public static void main(String[] args)
     {
-        // TODO
+        Skeleton s = new Skeleton();
+        s.start();
     }
     
     public void start()
     {
-        // TODO
+        int numPlayers = getPlayerNumber();
+        Simulation simulation = new Simulation();
+        loop();
+    }
+
+    private void loop(Simulation simulation){
+        while(true){
+            Robot next = simulation.getNextRobot();
+            modifySpeed(next);
+            placeBlob(next);
+            simulation.doJump();
+        }
+    }
+
+    private float getFloat(String message){
+      //TODO
+      return 0f;
+    }
+
+    private int getInt(String message){
+      //TODO
+      return 1;
+    }
+
+    private char getChar(StringMessage){
+      //TODO
+      return 'n';
+    }
+
+    private void modifySpeed(Robot r){
+      //TODO
+
+    }
+
+    private void placeBlob(Robot r){
+      //TODO
+
+    }
+
+    private int getPlayerNumber(){
+        //TODO
+        return 1;            
     }
     
     public Skeleton()
