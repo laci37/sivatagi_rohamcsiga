349b7d6c489a268d6fbd47aeca24ae95b1f379dd
Kristály Márk <k.marko93@gmail.com>
2015-03-14 21:10:10 +0100

    Skeleton osztály létrehozva

---
+++ Skeleton.java
@@ -0,0 +1,19 @@
+package sivatagi_rohamcsiga;
+
+public class Skeleton {
+
+	public static void main(String[] args)
+	{
+		// TODO
+	}
+	
+	public void start()
+	{
+		// TODO
+	}
+	
+	public Skeleton()
+	{
+		
+	}
+}
