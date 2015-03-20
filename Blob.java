ca30bf1ecfd5bfdd41c144be708b6d5f8e927897
László Mérő <laci37@gmail.com>
2015-03-20 18:59:55 +0100

    formázás

--- Blob.java
+++ Blob.java
@@ -1,8 +1,8 @@
 package sivatagi_rohamcsiga;
 //A foltokat megvalósító osztály.
 abstract class Blob extends Circle {
-	// Az osztály konstrukra, mely a megadott pozíción létrehoz egy új foltot.
-	public Blob(Vector location) {
+    // Az osztály konstrukra, mely a megadott pozíción létrehoz egy új foltot.
+    public Blob(Vector location) {
         super(location, 10f);
     }
 	//A folt hatásnak bekövetkezéséért felelős függvény, melyet a leszármazott osztályoknak kell megvlósítani.
